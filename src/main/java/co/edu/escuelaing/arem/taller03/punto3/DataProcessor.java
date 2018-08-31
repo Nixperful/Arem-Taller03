dcd78db75f59932585a3fa6f88b1ec9ad12f6503
Nixperful <nicolasosorioarias@hotmail.com>
2018-08-31 14:28:23 -0500

    Taller 3 Punto 3

---
+++ src/main/java/co/edu/escuelaing/arem/taller03/punto3/DataProcessor.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.escuelaing.arem.taller03.punto3;
+
+/**
+ *
+ * @author estudiante
+ */
+class DataProcessor {
+
+    public DataProcessor() {
+    }
+
+    public Float square(Float inputData) {
+        System.out.println("Petición de Duplicar: "+inputData);
+        return inputData*inputData;  
+    }
+    
+}
